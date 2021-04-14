e9b59b4b48814ef1ad65982da67df3ed437aaf50
IselaFraire <isewini@gmail.com>
2021-04-14 15:12:05 -0500

    Termine la aplicacion Gif

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,22 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SerchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class GifsService {
 
-  private apiKey   : string = 'PHtrNKm2av5NNzI6TWMYfnLB80JjqbyE';
-  private _historial: string[] = [];
+  private apiKey     : string = 'PHtrNKm2av5NNzI6TWMYfnLB80JjqbyE';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _historial : string[] = [];
 
-  //Cambiar any por su tipo correspondiente
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial(){
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient ){}
+  constructor(private http: HttpClient ){
+
+    //Devolver a su tipo original, si es vacio regresa arreglo vacio
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+
+    this.resultados = JSON.parse(localStorage.getItem('resultado')! ) || [];
+  }
 
   buscarGifs(query: string = ''){
     //Convertir a minuscula el string
@@ -29,12 +37,21 @@ export class GifsService {
       //Cortar el tamaño del arreglo a 10
       this._historial = this._historial.splice(0,10);
 
+      //Grabar en el local storage del navegador
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=PHtrNKm2av5NNzI6TWMYfnLB80JjqbyE&q=${ query }&limit=10`)
-      .subscribe((resp: any) =>{
+    const params = new HttpParams()
+          .set('api_key', this.apiKey)
+          .set('limit', '10')
+          .set('q', query);
+
+    this.http.get<SerchGifsResponse>(`${ this.servicioUrl }/search`, { params } )
+      .subscribe(resp  =>{
         console.log(resp.data);
         this.resultados = resp.data;
+        localStorage.setItem('resultado', JSON.stringify(this.resultados));
       });
   }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -13,4 +13,8 @@ export class SidebarComponent {
   get historial(){
     return this.gifsService.historial;
   }
+
+  buscar(termino: string){
+    this.gifsService.buscarGifs(termino);
+  }
 }
